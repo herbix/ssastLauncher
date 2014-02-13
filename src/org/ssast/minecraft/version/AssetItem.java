58d7f078f55f106bccd209a42f8d70d4cdff6700
chaos <chaos@36933577-587d-6f4b-8a0a-cef6ce05ccb4>
2014-02-13 12:14:38 +0000

    1.6.0续

---
+++ src/org/ssast/minecraft/version/AssetItem.java
@@ -0,0 +1,46 @@
+package org.ssast.minecraft.version;
+
+import org.json.JSONObject;
+import org.ssast.minecraft.Config;
+
+public class AssetItem {
+	
+	private String name;
+	private String hash;
+	private int size;
+	
+	public AssetItem(JSONObject json, String name) {
+		this.name = name;
+		this.hash = json.getString("hash");
+		this.size = json.getInt("size");
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public String getKey() {
+		return hash.substring(0, 2) + "/" + hash;
+	}
+
+	public String getTempFilePath() {
+		return Config.TEMP_DIR + Config.MINECRAFT_OBJECTS_PATH + "/" + getKey();
+	}
+
+	public String getRealFilePath() {
+		return Config.gamePath + Config.MINECRAFT_OBJECTS_PATH + "/" + getKey();
+	}
+
+	public String getFullUrl() {
+		return Config.MINECRAFT_RESOURCE_BASE + "/" + getKey();
+	}
+
+	public String getVirtualPath() {
+		return Config.gamePath + Config.MINECRAFT_VIRTUAL_PATH + "/" + getName();
+	}
+	
+}
